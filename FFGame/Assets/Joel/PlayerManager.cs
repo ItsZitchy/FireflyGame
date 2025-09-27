c8b5585e0ed493e47fc9f09f01f83cda3bc343fe
Elefphant <joel.hultsborn@gmail.com>
2025-09-27 03:56:45 +0200

    commit

--- FFGame/Assets/Joel/PlayerManager.cs
+++ FFGame/Assets/Joel/PlayerManager.cs
@@ -4,6 +4,7 @@ public class PlayerManager : MonoBehaviour
 {
     public float moveSpeed = 4f;
     public float jumpForce = 5f;
+    public float onWall = 0;
 
     private Rigidbody2D rb;
 
@@ -36,16 +37,21 @@ public class PlayerManager : MonoBehaviour
     public void MovementLogic()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
+
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
 
         if (isWalled(wallLayer))
         {
             rb.gravityScale = 0f;
+
+            onWall = 1f;
         }
         else
         {
             rb.gravityScale = 1f;
+            
+            onWall = 0f;
         }
     }
 
