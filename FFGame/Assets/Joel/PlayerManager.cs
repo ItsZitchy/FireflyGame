978125e3b83450aad13fdd835413779412f38643
Elefphant <joel.hultsborn@gmail.com>
2025-09-27 06:29:55 +0200

    movementors

--- FFGame/Assets/Joel/PlayerManager.cs
+++ FFGame/Assets/Joel/PlayerManager.cs
@@ -3,31 +3,49 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public float moveSpeed = 4f;
-    public float jumpForce = 5f;
-    public float onWall = 0;
+    public float jumpForce = 10f;
+    public float maxVerticalSpeed = 10f;
 
     private Rigidbody2D rb;
 
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
+
     public LayerMask wallLayer;
+    public float wallCheckRadius = 0.1f;
     public Transform LWallCheck;
     public Transform RWallCheck;
 
-    public float wallCheckRadius = 0.1f;
+
+    public float maxStamina = 100f;
+    public float staminaDrainRate = 40f;
+    public float staminaRegenRate = 50f;
+
+    private float currentStamina;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        currentStamina = maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded(groundLayer);
         isWalled(wallLayer);
         MovementLogic();
         flyLogic();
     }
 
+    private bool isGrounded(LayerMask groundLayer)
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
     private bool isWalled(LayerMask wallLayer)
     {
         return Physics2D.OverlapCircle(LWallCheck.position, wallCheckRadius, wallLayer)
@@ -37,29 +55,45 @@ public class PlayerManager : MonoBehaviour
     public void MovementLogic()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-
-        if (isWalled(wallLayer))
+        if (isWalled(wallLayer)) 
         {
             rb.gravityScale = 0f;
+            jumpForce = 0f;
 
-            onWall = 1f;
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, verticalInput * moveSpeed);
         }
         else
         {
             rb.gravityScale = 1f;
-            
-            onWall = 0f;
+            jumpForce = 10f;
+
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        }
+
+        if (isGrounded(groundLayer) || isWalled(wallLayer))
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            currentStamina = Mathf.Min(currentStamina, maxStamina);
+        }
+        else
+        {
+            currentStamina -= (staminaDrainRate /2) * Time.deltaTime;
         }
     }
 
     public void flyLogic()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetButton("Jump") && currentStamina > 0f)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+        }
+
+        if (rb.linearVelocity.y > maxVerticalSpeed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, maxVerticalSpeed);
         }
     }
 }
